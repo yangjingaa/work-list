b9fae63e10f8b027db77c87865c308cab96e8b10
517725252@qq.com <18633917479@mmears.com>
2018-10-30 11:45:04 +0800

    1

--- src/web/game2d/index.js
+++ src/web/game2d/index.js
@@ -1,22 +1,12 @@
 import React, {Component} from 'react'
 
-import {judgeDataType} from "../../utils/tools";
 
 class Game2d extends Component {
 
 
-    textObjectKey = () => {
-        const arr = [1, 2, 3];
-        arr.find(value => {
-            console.log(value);
-        });
-        const obj = Object.prototype.toString.call("name");
-        const res = judgeDataType("ssd", "string");
-        console.log(res);
-    };
+
 
     render() {
-        const a = this.textObjectKey();
         return (
             <div>
                 <p>2D游戏</p>
--- src/web/react-pac/index.js
+++ src/web/react-pac/index.js
@@ -5,8 +5,7 @@ class ReactPac extends Component {
     render() {
         return (
             <div>
-                <p>Reac      add .
-                    tPac</p>
+                <p>Reac</p>
             </div>
         );
     }
