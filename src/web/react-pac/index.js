3c41ea3cb01da3f04736a4a44adbe6f55067cd11
517725252@qq.com <18633917479@mmears.com>
2018-10-30 15:00:03 +0800

    remote  rename

--- src/web/react-pac/index.js
+++ src/web/react-pac/index.js
@@ -5,12 +5,21 @@ class ReactPac extends Component {
     render() {
         return (
             <div>
+
                <p>master1</p>
                <p>master2</p>
                <p>master3</p>
                <p>test21</p>
                <p>test22</p>
                <p>test23</p>
+                <p>test-1</p>
+                <p>Reac</p>
+                <p>Reac</p>
+                <p>Reac</p>
+                <p>Reac</p>
+                <p>Reac</p>
+                <p>Reac</p>
+                <p>Reac</p>
             </div>
         );
     }
