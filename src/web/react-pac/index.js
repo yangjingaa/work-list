df484489a572a1767808b302c02bc3963be13e74
517725252@qq.com <18633917479@mmears.com>
2018-11-05 19:24:31 +0800

    11

--- src/web/react-pac/index.js
+++ src/web/react-pac/index.js
@@ -14,31 +14,33 @@ class ReactPac extends Component {
                 <p>master1</p>
                 <p>master2</p>
                 <p>master2</p>
-                <p>master3</p>
-                <p>test21</p>
-                <p>master3</p>
+                <p>master3</p>                <p>master3</p>
+
                 <p>test21</p>
 
                 <p>master2</p>
-                <p>master3</p>
                 <p>test21</p>
-               <p>master1</p>
                <p>master2</p>
                <p>master3</p>
-               <p>test21</p>
-               <p>test22</p>
+
+                <p>test22</p>
+
+                <p>test21</p>
                <p>test23</p>
                 <p>test-1</p>
                 <p>Reac</p>
-                <p>text-1</p>
+                <p>text-1</p>               <p>test22</p>
                 <p>text-1</p>
                 <p>teReacxt-1</p>
                 <p>text</p>
                 <p>ReReacc</p>
+
                 <p>Reac</p>
                 <p>ReReacac</p>
-                <p>maReacster</p>
                 <p>master-2</p>
+
+                <p>maReacster</p>
+
                 <p>masteReacr-2</p>
                 <p>masReacter-2</p>
             </div>
